6e6db4b09a2aab8c79d7605ff38ee15447e435cf
Håkon Lia <hakon.lia@komplett.com>
2023-12-01 16:59:41 +0100

    Finished day 1

--- Day1/Program.cs
+++ Day1/Program.cs
@@ -1,3 +1,61 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿void Day1()
+{
+    var lines = File.ReadLines("../../../input.txt");
 
-Console.WriteLine("Hello, World!");
+    var count = 0;
+    
+    foreach (var line in lines)
+    {
+        // var numberAsCharacters = GetNumberFromLinePart1(line);
+        var numberAsCharacters = FindNumber(line, true) + FindNumber(line, false);
+        count += int.Parse(numberAsCharacters);
+    }
+    
+    Console.WriteLine(count);
+}
+
+string GetNumberFromLinePart1(string line) => line.First(char.IsDigit) + line.Reverse().First(char.IsDigit).ToString();
+
+
+string FindNumber(string line, bool findFirst)
+{
+    string iteratedWord = "";
+
+    var numbers = new Dictionary<string, string>
+    {
+        { "one", "1" },
+        { "two", "2" },
+        { "three", "3" },
+        { "four", "4" },
+        { "five", "5" },
+        { "six", "6" },
+        { "seven", "7" },
+        { "eight", "8" },
+        { "nine", "9" }
+    };
+
+    IEnumerable<char> characters = findFirst ? line : line.Reverse();
+    
+    foreach (var character in characters)
+    {
+        if (char.IsDigit(character))
+        {
+            return character.ToString();
+        }
+
+        iteratedWord = findFirst ? iteratedWord + character : character + iteratedWord;
+
+        foreach (var number in numbers)
+        {
+            if (iteratedWord.Contains(number.Key))
+            {
+                return number.Value;
+            }
+        }
+    }
+
+    throw new Exception("No numbers found");
+}
+
+
+Day1();
